e15190850d65c2c9910988475047ea2096279bc7
Janei <18485105+janeijones@users.noreply.github.com>
2021-03-14 22:38:34 -0500

    Local storage persisted for all text areas

--- Develop/script.js
+++ Develop/script.js
@@ -50,5 +50,14 @@ var saveBtnEl = $('.btn')
         localStorage.setItem(timeId, textAreaVal);
        });
 
-       $('#9 textarea').val(localStorage.getItem('9'));
+        $('#9 textarea').val(localStorage.getItem('9'));
+        $('#10 textarea').val(localStorage.getItem('10'));
+        $('#11 textarea').val(localStorage.getItem('11'));
+        $('#12 textarea').val(localStorage.getItem('12'));
+        $('#13 textarea').val(localStorage.getItem('13'));
+        $('#14 textarea').val(localStorage.getItem('14'));
+        $('#15 textarea').val(localStorage.getItem('15'));
+        $('#16 textarea').val(localStorage.getItem('16'));
+        $('#17 textarea').val(localStorage.getItem('17'));
+
       
